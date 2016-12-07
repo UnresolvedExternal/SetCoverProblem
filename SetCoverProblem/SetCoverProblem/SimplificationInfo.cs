37bfaf20846ba929cad3587d15cd26872917a8f0
UnresolvedExternal <vmi1401bsv04@gmail.com>
2016-12-08 03:41:45 +0600

    Simplificator released

---
+++ SetCoverProblem/SetCoverProblem/SimplificationInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace SetCoverProblem
+{
+	class SimplificationInfo
+	{
+		public readonly List<int> ColumnsExcluded;
+		public readonly List<int> RowsExcluded;
+		public readonly List<int> ColumnsInSolution;
+
+		public SimplificationInfo(List<int> columnsExcluded, List<int> rowsExcluded, List<int> columnsInSolution)
+		{
+			ColumnsExcluded = columnsExcluded;
+			RowsExcluded = rowsExcluded;
+			ColumnsInSolution = columnsInSolution;
+		}
+	}
+}
